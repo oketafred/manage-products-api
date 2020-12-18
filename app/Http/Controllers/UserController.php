c6c2a9449e93624a1b14f2d64ac789822586f440
Oketa Fred <oketafred@gmail.com>
2020-12-18 16:10:28 +0300

    Added API Resource Methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,51 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
     public function index()
     {
         return User::all();
+    }
+
+    public function show($id)
+    {
+        return User::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $user = User::create([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json($user, Response::HTTP_CREATED);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+
+        $user->update([
+            'first_name' => $request->input('first_name'),
+            'last_name' => $request->input('last_name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json($user, Response::HTTP_ACCEPTED);
+    }
+
+    public function destroy($id)
+    {
+        User::destroy($id);
+
+        return response()->json(null, Response::HTTP_NO_CONTENT);
     }
 }
