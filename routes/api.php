c3f58166fcca1150fdfadfbe773802c064fc5360
Oketa Fred <oketafred@gmail.com>
2020-12-27 17:47:31 +0300

    Added Login Route

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('login', [AuthController::class, 'login']);
 
 Route::apiResource('/users', UserController::class);
