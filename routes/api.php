14923586900121adb2bafbb9c564d480ea99fd4d
Oketa Fred <oketafred@gmail.com>
2020-12-27 22:55:51 +0300

    Added Middleware to Users API Endpoints

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
 
-Route::apiResource('/users', UserController::class);
+Route::group(['middleware' => 'auth:api'], function () {
+  Route::apiResource('/users', UserController::class);
+});
