ef9b6cd935cba8612eedfcf4cdc9fda7f81e031c
Oketa Fred <oketafred@gmail.com>
2020-12-30 13:53:19 +0300

    Added User Register and User Update Endpoints

--- routes/api.php
+++ routes/api.php
@@ -15,8 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::group(['middleware' => 'auth:api'], function () {
-  Route::apiResource('/users', UserController::class);
+    Route::get('/user', [UserController::class, 'user']);
+    Route::put('/users/info', [UserController::class, 'updateInfo']);
+    Route::put('/users/password', [UserController::class, 'updatePassword']);
+    Route::apiResource('/users', UserController::class);
 });
